8bf4bc44575c9117176655840db2104635a4f9d5
Sultan <ahmad.sultan.2777@gmail.com>
2023-11-28 19:54:53 +0700

    Rev - 28/11/2023 Fixed locale session issue for first time user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Categories;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class HomeController extends Controller
 {
-    function index() {
+    function index()
+    {
+        if (session('locale') == null) {
+            session(['locale' => App::currentLocale()]);
+        }
         $categories = Categories::all();
         return view('index', [
             'categories' => $categories
