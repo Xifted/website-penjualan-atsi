7494f045b7ac096360c4f5eb79a21df73f3dfa86
Sultan <ahmad.sultan.2777@gmail.com>
2023-12-24 21:17:38 +0700

    Rev 24/12/2023 - Made navbar sticky instead of fixed

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<header id="header" class="header fixed-top bg-white" data-scrollto-offset="0">
+<header id="header" class="header sticky-top bg-white" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between px-5">
         <a href="/" class="logo d-flex align-items-center scrollto me-auto me-lg-0 ps-5">
             <!-- Uncomment the line below if you also wish to use an image logo -->
@@ -10,33 +10,38 @@
         <nav id="navbar" class="navbar">
             <ul>
                 <li>
-                    <a class="nav-link" href="/#hero">{{ session()->get('locale') == 'en' ? 'Home Page' : 'Halaman Beranda' }}</a>
+                    <a class="nav-link"
+                        href="/#hero">{{ session()->get('locale') == 'en' ? 'Home Page' : 'Halaman Beranda' }}</a>
                 </li>
                 <li>
-                    <a class="nav-link" href="/#about">{{ session()->get('locale') == 'en' ? 'About Us' : 'Tentang Kami' }}</a>
+                    <a class="nav-link"
+                        href="/#about">{{ session()->get('locale') == 'en' ? 'About Us' : 'Tentang Kami' }}</a>
                 </li>
                 <li class="dropdown">
                     <a href="#">
-                        <span class="nav-link">{{ session()->get('locale') == 'en' ? 'Affiliate' : 'Afiliasi' }}</span> 
+                        <span class="nav-link">{{ session()->get('locale') == 'en' ? 'Affiliate' : 'Afiliasi' }}</span>
                         <i class="bi bi-chevron-down dropdown-indicator"></i>
                     </a>
                     <ul>
-                      <li><a href="#">PT. OMIC CONSULTING SERVICES INDONESIA</a></li>
-                      <li><a href="#">HQTS</a></li>
-                      <li><a href="#">GLOBAL QUALITY TECHNOLOGY GROUP (GQ)</a></li>
+                        <li><a href="#">PT. OMIC CONSULTING SERVICES INDONESIA</a></li>
+                        <li><a href="#">HQTS</a></li>
+                        <li><a href="#">GLOBAL QUALITY TECHNOLOGY GROUP (GQ)</a></li>
                     </ul>
-                  </li>
+                </li>
                 <li>
-                    <a class="nav-link" href="/#services">{{ session()->get('locale') == 'en' ? 'Our Services' : 'Layanan Kami' }}</a>
+                    <a class="nav-link"
+                        href="/#services">{{ session()->get('locale') == 'en' ? 'Our Services' : 'Layanan Kami' }}</a>
                 </li>
                 {{-- <li>
                     <a class="nav-link" href="/#wch">{{ session()->get('locale') == 'en' ? 'Experince' : 'Pengalaman' }}</a>
                 </li> --}}
                 <li>
-                    <a class="nav-link" href="/#contact">{{ session()->get('locale') == 'en' ? 'Contact Us' : 'Kontak Kami' }}</a>
+                    <a class="nav-link"
+                        href="/#contact">{{ session()->get('locale') == 'en' ? 'Contact Us' : 'Kontak Kami' }}</a>
                 </li>
                 <li>
-                    <a class="nav-link" href="/products#products">{{ session()->get('locale') == 'en' ? 'Products' : 'Produk' }}</a>
+                    <a class="nav-link"
+                        href="/products#products">{{ session()->get('locale') == 'en' ? 'Products' : 'Produk' }}</a>
                 </li>
                 <li>
                     <select class="form-select changeLang border-0" style="color: var(--color-primary)">
@@ -50,6 +55,7 @@
         </nav>
         <!-- .navbar -->
 
-        <a class="btn-getstarted scrollto" href="#">{{ session()->get('locale') == 'en' ? 'Get Started' : 'Gabung Sekarang' }}</a>
+        <a class="btn-getstarted scrollto"
+            href="#">{{ session()->get('locale') == 'en' ? 'Get Started' : 'Gabung Sekarang' }}</a>
     </div>
 </header>
