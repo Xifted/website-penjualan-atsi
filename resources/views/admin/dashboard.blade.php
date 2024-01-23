e8f58e51b6fc414be987e8f972b654a3e871d2c1
Vincent <oneisfirst01@gmail.com>
2024-01-23 11:23:54 +0700

    ignore

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -550,7 +550,7 @@
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="{{ asset('/assets/soft-ui/js/soft-ui-dashboard.min.js?v=1.0.7') }}"></script>
-    <script src="{{ asset('/js/admin/dashboard.js') }}"></script>
+    <script src="{{ asset('/assets/soft-ui/js/soft-ui-dashboard.js') }}"></script>
 </body>
 
 </html>
